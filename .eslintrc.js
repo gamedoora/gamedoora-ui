34a0700c066908e1f351b19a953190f25a528036
hybridx <hybridx@gmail.com>
2022-04-16 22:49:15 +0530

    feat(46) - Remove eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,25 +5,14 @@ module.exports = {
   },
   extends: [
     "plugin:vue/vue3-essential",
-    "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/explicit-module-boundary-types": ["off"],
   },
   overrides: [
     {
