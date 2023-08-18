0e930a805f4c9516802ce688901acde9033920ad
hybridx <hybridx18@gmail.com>
2023-08-19 02:41:42 +0530

    Added files and directory along with authcontext

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
-      <Link href={'/login'} className="text-[32px]">Goto login</Link>
+    <main className="grid">
+      <Link href={'/login'} className="text-[32px]">
+        Goto login
+      </Link>
+      <Link href={'/sign-up'} className="text-[32px]">
+        Goto Sign-up
+      </Link>
+      <Link href={'/profile/hybridx'} className="text-[32px]">
+        Goto {`Hybridx's`} profile
+      </Link>
     </main>
   );
 }
---
+++ src/app/profile/[slug]/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <main>You project page is loading...</main>;
+}
