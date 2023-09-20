5224e86b64238480568e81ea95d0a5e5bb9bf3c5
hybridx <hybridx18@gmail.com>
2023-09-20 12:01:46 +0530

    Fixed build issues

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,6 @@
-import NextAuth from 'next-auth/next';
-import GitHubProvider from 'next-auth/providers/github';
+import NextAuth from "next-auth/next";
+import { authOptions } from "./auth";
 
-export const authOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? '',
-      clientSecret: process.env.GITHUB_SECRET ?? '',
-    }),
-  ],
-};
-
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
