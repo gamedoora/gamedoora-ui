a3b49b018e9151bb1cdbeaf8883225e9fe06e96d
hybridx <hybridx18@gmail.com>
2023-10-08 10:53:53 +0530

    Added keycloak provider in gamedoora auth providers, enabling keycloak

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -1,10 +1,16 @@
 import { NextAuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
+import KeycloakProvider from 'next-auth/providers/keycloak';
 export const authOptions: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID ?? '',
       clientSecret: process.env.GITHUB_SECRET ?? '',
+    }),
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_ID ?? '',
+      clientSecret: process.env.KEYCLOAK_SECRET ?? '',
+      issuer: process.env.KEYCLOAK_ISSUER ?? '',
     }),
   ],
 };
