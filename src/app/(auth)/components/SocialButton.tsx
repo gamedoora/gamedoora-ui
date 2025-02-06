15b71b69d9eafc96ff8518e9abd1f51846e30058
hybridx <hybridx18@gmail.com>
2025-02-07 01:49:45 +0530

    added sign in button

--- src/app/(auth)/components/SocialButton.tsx
+++ src/app/(auth)/components/SocialButton.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { Button } from '@mui/material';
 import Image from 'next/image';
-import React from 'react';
 
 interface SocialButtonProps {
   disabled?: boolean;
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -119,9 +119,15 @@ export default function SignInTest() {
                       iconPath="git.svg"
                       socialName="GitHub"
                       onClick={() => {
-                        signIn();
+                        signIn('github');
                       }}
                     />
+                    <button
+                      className="w-24 px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                      onClick={() => signIn()}
+                    >
+                      Sign In
+                    </button>
                   </div>
                 </div>
               </form>
