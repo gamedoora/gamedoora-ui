205bc6e9801b45ad635568d2e3114549f3019074
hybridx <hybridx18@gmail.com>
2025-02-07 01:52:56 +0530

    removed button

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -124,7 +124,7 @@ export default function SignInTest() {
                     />
                     <button
                       className="w-24 px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50"
-                      onClick={() => signIn()}
+                      
                     >
                       Sign In
                     </button>
